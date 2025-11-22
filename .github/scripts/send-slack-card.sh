c2b85f0f02f05e0bf194ee8028c325b3b4a3c6c6
jeremyyymrcs <marcosjeremaya@gmail.com>
2025-11-22 23:20:08 +0800

    Add slack webhook

---
+++ .github/scripts/send-slack-card.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+
+run_url="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+
+json_payload=$(cat <<EOF
+{
+  "blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*Test Summary:*"
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*Status:* ${RESULT_STATUS}\n✅ Passed: ${PASSED}\n❌ Failed: ${FAILED}\n🧪 Total Tests: ${TOTAL}"
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*Details:*"
+      }
+    },
+    {
+      "type": "section",
+      "fields": [
+        {
+          "type": "mrkdwn",
+          "text": "📂 *Branch:*\n${BRANCH}"
+        },
+        {
+          "type": "mrkdwn",
+          "text": "🔢 *Commit Hash:*\n${COMMIT_HASH}"
+        },
+        {
+          "type": "mrkdwn",
+          "text": "💬 *Commit Message:*\n${COMMIT_MESSAGE}"
+        },
+        {
+          "type": "mrkdwn",
+          "text": "👤 *Actor:*\n${ACTOR}"
+        },
+        {
+          "type": "mrkdwn",
+          "text": "🕒 *Date/Time:*\n${DATE_TIME}"
+        }
+      ]
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "📄 <${run_url}|View Allure Report>"
+      }
+    }
+  ]
+}
+EOF
+)
+
+curl -X POST "$SLACK_WEBHOOK_URL" \
+     -H "Content-Type: application/json" \
+     -d "$json_payload"
